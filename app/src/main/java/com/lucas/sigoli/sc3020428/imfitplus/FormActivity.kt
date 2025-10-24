8e3bf3f195aefa07c0fc7d7a9a0a74a8f4104ccc
LVSigoli <lucasviniciussigoli@gmail.com>
2025-10-23 22:40:56 -0300

    feat: Implement back button to forms toolbar

--- app/src/main/java/com/lucas/sigoli/sc3020428/imfitplus/FormActivity.kt
+++ app/src/main/java/com/lucas/sigoli/sc3020428/imfitplus/FormActivity.kt
@@ -1,12 +1,11 @@
 package com.lucas.sigoli.sc3020428.imfitplus
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.lucas.sigoli.sc3020428.imfitplus.databinding.ActivityFormBinding
-import com.lucas.sigoli.sc3020428.imfitplus.databinding.ActivityMainBinding
 
-class FormActivity: AppCompatActivity() {
+class FormActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFormBinding
 
@@ -14,11 +13,24 @@ class FormActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityFormBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar.toolbar)
-
         supportActionBar?.title = "Informações pessoais"
+
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
