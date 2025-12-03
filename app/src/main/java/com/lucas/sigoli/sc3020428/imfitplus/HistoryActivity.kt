be0b735b1c0765ccabc7d98d5327be36acd1e6bd
LVSigoli <lucasviniciussigoli@gmail.com>
2025-12-03 19:40:37 -0300

    feat: Implement base History activity structure

---
+++ app/src/main/java/com/lucas/sigoli/sc3020428/imfitplus/HistoryActivity.kt
@@ -0,0 +1,51 @@
+package com.lucas.sigoli.sc3020428.imfitplus
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.lucas.sigoli.sc3020428.imfitplus.databinding.ActivityHistoryBinding
+
+
+class HistoryActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityHistoryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHistoryBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        setContentView(binding.root)
+
+        setupToolbar(binding)
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onStart(){
+        super.onStart()
+
+    }
+
+
+    // Utils
+    private fun setupToolbar(binding: ActivityHistoryBinding) {
+        setSupportActionBar(binding.toolbar.toolbar)
+
+        supportActionBar?.title = "Histórico"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
